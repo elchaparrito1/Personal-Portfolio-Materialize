dbf3c36d17f36aacf2e582fc21da12c3977664d7
Mitchell Waite <mitchellw11@us.usana.com>
2018-09-29 15:57:03 -0600

    Updated Portfolio

--- assets/logic.js
+++ assets/logic.js
@@ -1,3 +1,27 @@
 $(document).ready(function(){
     $('.parallax').parallax();
 });
+
+//Recall the vanilla javascript version:
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.parallax');
+//     var instances = M.Parallax.init(elems, options);
+//   });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, {
+      direction: 'top'
+    });
+  });
+
+  
+  $(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').modal();
+  });
+
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
